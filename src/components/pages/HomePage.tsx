1bdedfc9e1f6828dafe543a221e2f4b9c663a6c3
Emiel <emiel.delaey@student.howest.be>
2023-11-06 13:36:18 +0100

    added api connection

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,16 +1,4 @@
-import {useState} from "react";
-
-function CategoryBar() {
-
-    const [categories, setCategories] = useState(["Sports", "Music"]);
-
-    return <header>
-        <ul>
-            {categories.map(c => <li><button>{c}</button></li>)}
-        </ul>
-    </header>;
-}
-
+import CategoryBar from "../CategoryBar";
 export default function HomePage() {
     return <>
         <CategoryBar></CategoryBar>
