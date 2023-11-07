7b4169f2f90a2d5642434423561f308f3b594148
Seppe <seppe.van.den.broek@student.howest.be>
2023-11-07 12:08:18 +0100

    changed primary color

--- src/components/EventBlock.tsx
+++ src/components/EventBlock.tsx
@@ -5,5 +5,13 @@ type TEventBlockProps = {
 }
 
 export default function EventBlock({event}: TEventBlockProps) {
-    return event.name;
+    return <div className={"event"}>
+            <h2>{event.title}</h2>
+            <ul>
+                <li><p>{event.category}</p></li>
+                <li><p>{event.organiser}</p></li>
+                <li><p>{event.location}</p></li>
+            </ul>
+        <p>{event.id}</p>
+        </div>;
 }
--- src/components/common/events.tsx
+++ src/components/common/events.tsx
@@ -1,13 +1,13 @@
 import {TUser} from "./user.tsx";
-import {TCategory} from "./category.tsx";
-import {TLocation} from "./location.tsx";
+/*import {TCategory} from "./category.tsx";
+import {TLocation} from "./location.tsx";*/
 
 export type TEvent ={
     id: number;
     title: string;
     //eventType: string;
-    category: TCategory[];
+    category: string;
     organiser: string;
     attendees: TUser[];
-    location: TLocation[];
+    location: string;
 }
