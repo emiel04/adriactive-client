56ab02f3fa29559c85bf45bb759d08942c32e758
Fares Suhail <suhail.fares@student.howest.be>
2023-11-07 11:20:42 +0100

    implementing the safe zones on the map doesnt work yet

--- src/components/pages/MapPage.tsx
+++ src/components/pages/MapPage.tsx
@@ -1,31 +1,56 @@
 import * as ol from "ol"
 import {fromLonLat} from "ol/proj";
 import {OSM} from "ol/source";
-import {Tile} from "ol/layer";
+import {Tile, Vector} from "ol/layer";
+import {useState} from "react";
+import {Feature} from "ol";
+import {Point} from "ol/geom";
+import {Icon, Style} from "ol/style";
+
 
 function MapPage(){
-    function eventHandler(event){
-        event.target.disabled = true;
-        new ol.Map({
-            target: 'map',
-            layers: [
-                new Tile({
-                    source: new OSM()
-                })
-            ],
-            view: new ol.View({
-                center: fromLonLat([12.060, 45.0528]),
-                zoom: 15
-            })
-        });
-    }
 
+    const testLocations = [
+        {
+            coordinates : [12.060, 45.0528]
+        },
+        {
+            coordinates : [11.000, 45.0000]
+        }
+    ]
+
+    const [visible, setVisible] = useState(true);
+
+    function eventHandler(){
+
+    setVisible((prev) => !prev);
+
+    new ol.Map({
+        target: 'map',
+        layers: [
+            new Tile({
+                source: new OSM()
+            })
+        ],
+        view: new ol.View({
+            center: fromLonLat([12.060, 45.0528]),
+            zoom: 15
+        })
+    });
+    }
     return <>
-        <button onClick={eventHandler}>show map</button>
+        {visible && <button onClick={eventHandler}>show map</button>}
         <div id="map" style={{ width: '100%', height: '40rem' }}></div>;
     </>
 
+
 }
+
+
+    //
+
+
+
 export default MapPage;
 
 
