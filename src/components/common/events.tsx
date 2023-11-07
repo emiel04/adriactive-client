501a9bfc680f67558640c986ebd25a5146799b4e
Seppe <seppe.van.den.broek@student.howest.be>
2023-11-07 10:41:00 +0100

    added some types

--- src/components/common/events.tsx
+++ src/components/common/events.tsx
@@ -1,4 +1,13 @@
+import {TUser} from "./user.tsx";
+import {TCategory} from "./category.tsx";
+import {TLocation} from "./location.tsx";
+
 export type TEvent ={
     id: number;
-    name: string;
+    title: string;
+    //eventType: string;
+    category: TCategory[];
+    organiser: string;
+    attendees: TUser[];
+    location: TLocation[];
 }
