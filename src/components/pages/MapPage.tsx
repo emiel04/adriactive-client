7436f2bccc112fd4fb1e78505c0934909aa1a5a4
Seppe <seppe.van.den.broek@student.howest.be>
2023-11-08 15:15:19 +0100

    autoload but loads 2

--- src/components/pages/MapPage.tsx
+++ src/components/pages/MapPage.tsx
@@ -1,36 +1,42 @@
-import * as ol from "ol"
+import {useState, useEffect} from "react";
+import * as ol from "ol";
 import {fromLonLat} from "ol/proj";
 import {OSM} from "ol/source";
 import {Tile} from "ol/layer";
-import {useState} from "react";
 
-function MapPage(){
-
+function MapPage() {
     const [visible, setVisible] = useState(true);
 
-    function showMapHandler(){
+    useEffect(() => {
+        showMapHandler();
+    }, []);
+
+    function showMapHandler() {
+        removeMap();
+
         setVisible((prev) => !prev);
         new ol.Map({
-        target: 'map',
-        layers: [
-            new Tile({
-                source: new OSM()
+            target: 'map',
+            layers: [
+                new Tile({
+                    source: new OSM()
+                })
+            ],
+            view: new ol.View({
+                center: fromLonLat([12.060, 45.0528]),
+                zoom: 15
             })
-        ],
-        view: new ol.View({
-            center: fromLonLat([12.060, 45.0528]),
-            zoom: 15
-        })
         });
     }
-    return <>
-        {visible && <button onClick={showMapHandler}>show map</button>}
-        <div id="map" style={{width: '100%', height: '40rem'}}/>
-    </>
 
+    return (
+        <div id="map" style={{width: '100%', height: '40rem', display: visible ? 'block' : 'none'}} />
+    );
 
 }
 
+    function removeMap() {
+        return <div/>
+    }
+
 export default MapPage;
-
-
