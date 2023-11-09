3d4e5be13c5ceb44895e1e49539b6649e0793931
Emiel <emiel.delaey@student.howest.be>
2023-11-09 11:42:49 +0100

    added mui and mui-icons

--- src/components/common/404.tsx
+++ src/components/common/404.tsx
@@ -1,3 +1,8 @@
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
 export default function NotFound() {
-    return <p>404 Not found</p>
+    return <>
+        <AccountCircleIcon></AccountCircleIcon>
+        <p>404 Not found</p>
+    </>
 }
