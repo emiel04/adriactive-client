06ee1c9bea17a0d876c819bb2274bc811c648e83
Emiel <emiel.delaey@student.howest.be>
2023-11-09 11:43:34 +0100

    removed useless button

--- src/components/common/404.tsx
+++ src/components/common/404.tsx
@@ -1,8 +1,5 @@
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-
 export default function NotFound() {
     return <>
-        <AccountCircleIcon></AccountCircleIcon>
         <p>404 Not found</p>
     </>
 }
