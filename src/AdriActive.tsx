b223662228b691a893279599ea08044914536173
Emiel <emiel.delaey@student.howest.be>
2023-11-10 09:23:16 +0100

    trying new build for the server

--- src/AdriActive.tsx
+++ src/AdriActive.tsx
@@ -21,7 +21,7 @@ function AdriActive () {
             <Route path={"/map"} element={<MapPage/>}></Route>
             <Route path={"/myEvents"} element={<MyEventsPage/>}></Route>
             <Route path={"/notifications"} element={<p>Notifications</p>}></Route>
-            <Route path={"/start"} element={<StartPage/>}></Route>
+            <Route path={"/start"} element={<StartPage isEditing={false}/>}></Route>
             <Route path={"*"} element={<NotFound/>}></Route>
         </Routes>
         <Navbar></Navbar>
