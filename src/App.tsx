8e02f51005d2b4200dd4cf9816a00f03af27d6eb
Emiel <emiel.delaey@student.howest.be>
2023-11-15 17:13:42 +0100

    added websockets to application

--- src/App.tsx
+++ src/App.tsx
@@ -4,48 +4,27 @@ import AdriActive from "./AdriActive";
 import { CssVarsProvider } from '@mui/joy/styles';
 import NotFound from "./components/common/404";
 import {PrivateRoute} from "./components/PrivateRoute";
-import {useEffect} from "react";
-import EventBus from "@vertx/eventbus-bridge-client.js";
-import URI from "./api";
 import theme from "./assets/theme";
+import {WebSocketProvider} from "./components/context/WebSocketContext";
 
 
-const CHNL_TO_CLIENTS_BROADCAST = "events.to.users";
 
 function App() {
-
-    useEffect(() => {
-        const eb = new EventBus(URI + "/events");
-
-        eb.onopen = () => {
-            console.log("Opening");
-            eb.registerHandler(CHNL_TO_CLIENTS_BROADCAST, onMessage);
-        }
-
-        function onMessage(error: Error, message: any) {
-            if(error){
-                console.error(error);
-            }
-            console.log(message.body);
-        }
-        return () =>{
-            eb.close();
-        }
-    })
-
 
     return (
         <>
             <CssVarsProvider theme={theme}>
-                <Routes>
-                    <Route path={"/"} element={<Navigate to={"/app"}/>}></Route>
-                    <Route path="/app/*" element={
-                        <PrivateRoute>
-                            <AdriActive></AdriActive>
-                        </PrivateRoute>
-                    } />
-                    <Route path="*" element={<NotFound/>} />
-                </Routes>
+                <WebSocketProvider>
+                    <Routes>
+                        <Route path={"/"} element={<Navigate to={"/app"}/>}></Route>
+                        <Route path="/app/*" element={
+                            <PrivateRoute>
+                                <AdriActive></AdriActive>
+                            </PrivateRoute>
+                        } />
+                        <Route path="*" element={<NotFound/>} />
+                    </Routes>
+                </WebSocketProvider>
             </CssVarsProvider>
 
         </>
