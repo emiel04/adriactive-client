81ef33f581cc75d6dd2f9b867f39d54700bfe475
Delaey Emiel <emiel.delaey@student.howest.be>
2023-12-19 13:55:05 +0100

    added 404.css

--- src/components/common/404.tsx
+++ src/components/common/404.tsx
@@ -1,5 +1,6 @@
 // import {useLocation} from "react-router";
 import "../../assets/css/404.css"
+import {useNavigate} from "react-router-dom";
 
 export default function NotFound() {
     // const loc = useLocation();
@@ -8,9 +9,10 @@ export default function NotFound() {
     //     {/*<p>{loc.pathname}</p>*/}
     //     {/*<p>{JSON.stringify(loc)}</p>*/}
     // </>
+    const nav = useNavigate();
     return <div className="notfound">
         <h1>Page not found!</h1>
         <p>(╯°□°）╯︵ ┻━┻</p>
-        <a href="#">Go to home</a>
+        <a onClick={() => nav("/")}>Go to home</a>
     </div>
 }
