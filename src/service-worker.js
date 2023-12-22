2661ddac348d18c011d5f954fb2e27551097695a
Delaey Emiel <emiel.delaey@student.howest.be>
2023-12-22 18:01:54 +0100

    build

--- src/service-worker.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('push', event => {
-  const options = {
-    body: event.data.text(),
-    icon: '/assets/img/Logo-inverted.png',
-  };
-  event.waitUntil(
-    self.registration.showNotification('AdriActive', options)
-  );
-});
