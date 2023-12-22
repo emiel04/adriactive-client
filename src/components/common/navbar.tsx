0be333a78f59cd1f434694df8083827f7ae1cdd9
Delaey Emiel <emiel.delaey@student.howest.be>
2023-12-22 22:34:07 +0100

    updated my events page error css

--- src/components/common/navbar.tsx
+++ src/components/common/navbar.tsx
@@ -22,34 +22,34 @@ export default function Navbar() {
 
     return <nav className={"navbar"}>
         <ul>
-        <li>
-            <NavLink to={"/app/profile"} className={({isActive}) => isActive ? "active" : ""}>
-                <AccountCircleIcon></AccountCircleIcon>
-            </NavLink>
-        </li>
-        <li>
-            <NavLink to={"/app/home"} className={({isActive}) => isActive ? "active" : ""}>
-            Home
-            </NavLink>
-        </li>
-        <li>
-            <NavLink to={"/app/map"} className={({isActive}) => isActive ? "active" : ""}>
-            Map
-            </NavLink>
-        </li>
-        <li>
-            <NavLink to={"/app/events"} className={({isActive}) => isActive ? "active" : ""}>
-            Events
-            </NavLink>
-        </li>
+            <li>
+                <NavLink to={"/app/home"} className={({isActive}) => isActive ? "active" : ""}>
+                    Home
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to={"/app/map"} className={({isActive}) => isActive ? "active" : ""}>
+                    Map
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to={"/app/events"} className={({isActive}) => isActive ? "active" : ""}>
+                    Events
+                </NavLink>
+            </li>
 
-        <li>
-            <Badge badgeContent={count} variant="solid">
-            <NavLink to={"/app/notifications"} className={({isActive}) => isActive ? "active" : ""}>
-            Notifications
-            </NavLink>
-            </Badge>
-        </li>
+            <li>
+                <Badge badgeContent={count} variant="solid">
+                    <NavLink to={"/app/notifications"} className={({isActive}) => isActive ? "active" : ""}>
+                        Notifications
+                    </NavLink>
+                </Badge>
+            </li>
+            <li className={"navbar-end"}>
+                <NavLink to={"/app/profile"} className={({isActive}) => isActive ? "active" : ""}>
+                    <AccountCircleIcon></AccountCircleIcon>
+                </NavLink>
+            </li>
         </ul>
     </nav>;
 }
